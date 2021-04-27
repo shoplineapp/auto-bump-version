7dd7b2dd421f59994bb8bceb92e1ff0e92fca39f
Ivan <ivan@shoplineapp.com>
2021-04-27 11:37:21 +0800

    feat: add logger for debugging

--- pipe/tagging.py
+++ pipe/tagging.py
@@ -1,19 +1,23 @@
 import re
 
 from git import Repo
+from bitbucket_pipes_toolkit import get_logger
 
 
 class Tagging:
 
     def __init__(self, pipe):
         self.pipe = pipe
+        self.logger = get_logger()
 
     def run(self):
-        repo = Repo()
-        git = repo.git
+        git = Repo().git
 
         commit_id = re.search(r'commit (?P<commit_id>\w+)', git.log("-1"))['commit_id']
+        self.logger.info('commit id: {}'.format(commit_id))
+
         file_changes = git.diff_tree("--no-commit-id", "--name-only", '-r', commit_id).split("\n")
+        self.logger.info('file changes: {}'.format(file_changes))
 
         if self.pipe.get_variable('FILE_PATH') in file_changes:
             current_version = self.pipe.current_version()
