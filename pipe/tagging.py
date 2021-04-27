8c6a8cf743c44236a213400c65927a601d70e75f
Ivan <ivan@shoplineapp.com>
2021-04-27 10:30:23 +0800

    fix: tag version need to supply message

--- pipe/tagging.py
+++ pipe/tagging.py
@@ -13,5 +13,5 @@ class Tagging:
         repo = Repo()
         git = repo.git
         git.config(f"http.{self.pipe.env['BITBUCKET_GIT_HTTP_ORIGIN']}.proxy", 'http://host.docker.internal:29418/')
-        git.tag(a=current_version)
+        git.tag(a=current_version, m="auto tag version {} by bitbucket pipeline".format(current_version))
         git.push(tags=True)
