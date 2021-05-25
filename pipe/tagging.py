faa478cc0d0e0fc5f95c02563333e1e13315d1c9
Ivan <ivan@shoplineapp.com>
2021-05-25 16:15:03 +0800

    feat: add log

--- pipe/tagging.py
+++ pipe/tagging.py
@@ -24,12 +24,17 @@ class Tagging:
 
             max_tag = "0.0.0"
             tags = git.tag().split('\n')
+
+            self.logger.info('tags: {}'.format(tags))
+
             if tags:
                 tags = [t for t in tags if re.match(r"^\d+\.\d+\.\d+$", t)]
                 tags.sort(key=lambda s: list(map(int, s.split('.'))))
                 max_tag = tags[-1]
 
             current_version = self.pipe.current_version()
+            self.logger.info(f"max tag: {max_tag}")
+            self.logger.info(f"current version: {current_version}")
             if version.parse(current_version) > version.parse(max_tag):
                 git.config(f"http.{self.pipe.env['BITBUCKET_GIT_HTTP_ORIGIN']}.proxy", 'http://host.docker.internal:29418/')
                 git.tag(a=current_version, m="auto tag version {} by bitbucket pipeline".format(current_version))
